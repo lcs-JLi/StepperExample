296f5f8822463267e336a48e0eb78231a119abfc
John Li <jli@lcs.on.ca>
2026-04-20 14:21:51 -0400

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Xichen Li on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
